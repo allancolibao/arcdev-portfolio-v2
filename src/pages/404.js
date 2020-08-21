e40ca702554e0ebea9c2892cbfe722b6679f0b10
allancolibao <colibaoallanreyes@gmail.com>
2020-08-21 16:20:23 +0800

    modified 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,9 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import PageNotFound from "../images/svgs/page-not-found.svg";
 
 const NotFoundPage = ({ location }) => { 
 
@@ -10,7 +12,36 @@ const NotFoundPage = ({ location }) => {
     return(
     <Layout location={location}>
       <SEO title="404: Not Found | Allan Colibao - Front-end Developer"/>
-      <h1 class="text-xl md:text-3xl lg:text-4xl font-black text-gray-400 leading-tight my-16">Under construction, Sorry for the inconvenience</h1>
+      <div className="flex mx-auto items-center justify-center">
+        <PageNotFound className="h-auto my-16"/>
+      </div>
+      <div className="flex mx-auto items-center justify-center">
+      <Link to="/">
+          <button className="bg-transparent 
+          text-purple-400
+          text-xs
+          lg:text-xs 
+          xl:text-base 
+          font-semibold 
+          py-1
+          px-2
+          md:py-1 
+          md:px-3 
+          border 
+          rounded 
+          border-purple-400 
+          transition 
+          duration-500 
+          ease-in-out 
+          transform 
+          focus:outline-none
+          hover:border-transparent 
+          hover:bg-purple-500 
+          hover:text-gray-200">
+            {"<<"} Go back to home
+          </button>
+        </Link>
+      </div>
     </Layout>
   )
 
